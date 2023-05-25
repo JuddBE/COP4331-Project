08bf4ba93cb46806fc47f2f9ae6ea7018baef736
Noah Gregory <noahgregory@MacBook-Pro.local>
2023-05-24 22:06:56 -0400

    Search Contact works now

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -5,14 +5,41 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "fb", "123", "COP4331"); 
+	$conn = new mysqli("localhost", "fb", "123", "COP4331");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		// Add logic here for searching contacts
+		$stmt = $conn->prepare("select * from Contacts where FirstName like ? and UserID=?");
+		$contactName = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ss", $contactName, $inData["userId"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "Email" : "' . $row["Email"]. '", "Phone" : "' . $row["Phone"]. '", "dateCreated" : "' . $row["dateCreated"]. '"}';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
 	}
 
 	function getRequestInfo()
